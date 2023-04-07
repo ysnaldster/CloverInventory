dad29509601216d6559ab9553ee1fda25b0f7b9c
ysnaldster <ysnaldolopez@gmail.com>
2023-04-07 16:44:48 -0500

    Home view v1 with login

--- InventoryManager.Views/Startup.cs
+++ InventoryManager.Views/Startup.cs
@@ -24,7 +24,6 @@ public class Startup
         services.AddScoped<ProtectedSessionStorage>();
         services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
         services.AddScoped<UserService>();
-        services.AddControllers();
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
 
